10f1ed156e03adbcc7473d4c4f1028988571ea0b
Abhinav-Vishwakarma <avrk97945@gmail.com>
2025-11-10 10:19:25 +0530

    Added Login, Register, Dashboard, MainLayout Routes

---
+++ ReactWork/ShoppingCart/src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './components/Login';
+import Register from './components/Register';
+import Dashboard from './components/Dashboard';
+import MainLayout from './components/MainLayout';
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/login' element = {<Login />}></Route>
+          <Route path='/register' element = {<Register />}></Route>
+          <Route path='/dashboard' element = {<Dashboard />}></Route>
+          <Route path='/' element = {<MainLayout />}></Route>
+
+
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
